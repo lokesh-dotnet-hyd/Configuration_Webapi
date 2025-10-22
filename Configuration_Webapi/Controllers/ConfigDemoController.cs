b0a4e5cbd2277441eb3a3cd976f0ec534b1d327d
lokesh-dotnet-hyd <abhilokeshgoud92@gmail.com>
2025-10-22 17:54:37 +0530

    Update ConfigDemoController.cs

--- Configuration_Webapi/Controllers/ConfigDemoController.cs
+++ Configuration_Webapi/Controllers/ConfigDemoController.cs
@@ -1,1 +1,24 @@
-﻿t
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration_Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigDemoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration; 
+        public ConfigDemoController(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+        [HttpGet]
+        public IActionResult GetAppSettings()
+        {
+            var appName = _configuration["AppSettings:ApplicationName"];
+            var version= _configuration["AppSettings:Version"];
+            return Ok(new {appName, version});  
+        }
+    }
+}
